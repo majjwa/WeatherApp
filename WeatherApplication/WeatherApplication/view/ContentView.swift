24162b09b915739664102b3781be3b3a5b05e116
Marwa Maky <marwamaky50@gmail.com>
2024-08-27 11:34:56 +0300

    Update UI
    Update UI

--- WeatherApplication/WeatherApplication/view/ContentView.swift
+++ WeatherApplication/WeatherApplication/view/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by marwa maky on 27/08/2024.
 //
 import SwiftUI
+import Kingfisher
 
 struct ContentView: View, WeatherViewProtocol {
     @StateObject private var presenter = WeatherPresenter()
@@ -20,25 +21,57 @@ struct ContentView: View, WeatherViewProtocol {
     var fontColor: Color {
         currentHour >= 5 && currentHour < 18 ? .black : .white
     }
+    
     var body: some View {
         ZStack {
             Image(backgroundImage)
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
-            if let weather = presenter.weatherData {
-                VStack {
-                    Text("Location: \(weather.location.country)")
-                    Text("Temperature: \(weather.current.temp_c)°C")
+            
+            VStack {
+                if let weather = presenter.weatherData {
+                    VStack(spacing: 1) {
+                        Text("\(weather.location.name)")
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundColor(fontColor)
+                        
+                        Text("\(String(format: "%.f", weather.current.temp_c))°C")
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundColor(fontColor)
+                        
+                        Text("\(weather.current.condition.text)")
+                            .font(.largeTitle)
+                            .foregroundColor(fontColor)
+                        
+                        if let forecastDay = weather.forecast.forecastday.first {
+                            Text("H: \(String(format: "%.f", forecastDay.day.maxtemp_c))° L: \(String(format: "%.f", forecastDay.day.mintemp_c))°")
+                                .font(.largeTitle)
+                                .foregroundColor(fontColor)
+                        }
+                        
+                        KFImage(URL(string: "https:\(weather.current.condition.icon)"))
+                            .resizable()
+                            .scaledToFit() // Use scaledToFit to maintain aspect ratio
+                            .frame(width: 160, height: 100)
+                    }
+                    .padding()
+                    
+                    VStack {
+                        Text("3-DAY FORECAST")
+                            .font(.title2)
+                            .foregroundColor(fontColor)
+                        Divider().background(Color.black)  .frame(width: 300)                    }
+                    .padding()
+                } else {
+                    Text("Loading...")
                         .font(.largeTitle)
                         .foregroundColor(fontColor)
                 }
-                .padding()
-            } else {
-                Text("Loading...")
-                    .font(.largeTitle)
-                    .foregroundColor(fontColor)
             }
+            .padding(100)
         }
         .onAppear {
             presenter.view = self
